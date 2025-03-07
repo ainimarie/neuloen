ff9271bcc5f1ff58bdf40daba4d4cd1398ed6b73
ainimarie <ainimarie@gmail.com>
2025-03-07 23:41:55 +0200

    change pwa to full-screen

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,7 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: 'Pipolaskuri',
     description: 'Laskuri pipon kavennyssilmukoiden laskemiseen',
     start_url: '/',
-    display: 'standalone',
+    display: 'fullscreen',
     background_color: '#ffffff',
     theme_color: '#000000',
     icons: [
